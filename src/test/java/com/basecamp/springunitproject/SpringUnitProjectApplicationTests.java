5eba4c382a9f4386aaa4cf0865fca4df1f6cde48
JT <jttutor10@gmail.com>
2026-03-11 09:32:09 -0500

    Initial commit

---
+++ src/test/java/com/basecamp/springunitproject/SpringUnitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basecamp.springunitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUnitProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
